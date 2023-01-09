da544c627a69e267b0ce4c924375baec22496ec0
Elielder Oliveira <taganet.info@gmail.com>
2023-01-09 19:16:33 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+//Inicializando Nókia
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("11 12345-6789", "N95", "0123045607890", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+nokia.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone("11 98765-4321", "Iphone 8", "0012004500780", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+iphone.ReceberLigacao();
+
+Console.WriteLine("\n");
